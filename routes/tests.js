991d1cdaa3e04969d1e2d9248cc0988755a72765
Chrisgood2go <christopher.garcia0714@gmail.com>
2021-11-20 19:17:12 +0000

    2

--- routes/tests.js
+++ routes/tests.js
@@ -1,4 +1,29 @@
 const router = require('express').Router();
 
+const app = router()
+// var express = require('express')
+// var app = express()
+
+// // respond with "hello world" when a GET request is made to the homepage
+// app.get('/', function (req, res) {
+//   res.send('hello world')
+// })
+
+
+app.GET('/students',function(req, res){
+    
+});
+
+app.GET('/student/:id',function(req, res){
+    
+});
+
+router.POST('/sudents', function(req, res){
+    
+});
+
+router.PUT('/students/:id', function(req, res){
+    
+});
 
 module.exports = router;
