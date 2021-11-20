b18dfe26ca4a8a895abdb9e8ed898a1fbdd6b430
Chrisgood2go <christopher.garcia0714@gmail.com>
2021-11-20 20:47:50 +0000

    end sat session

--- routes/tests.js
+++ routes/tests.js
@@ -1,4 +1,49 @@
 const router = require('express').Router();
+// const Student = require('../db/models/student')
+const Test = require('../db/models/test')
 
+// router.get('/', async (req, res, next) => {
+//   try {
+//     const tests = await Test.findAll()
+//     res.send(tests)
+//   } catch (error) {
+//     next(error)
+//   }
+// })
+router.get('/', async (req, res, next)=>{
+    try {
+        const tests = await Test.findAll()
+        res.send(tests)
+    } catch (error) {
+        next(error)
+    }
+    
+})
+
+// router.get('/:id', async (req, res, next) => {
+//   try {
+//     let student = await Student.findByPk(req.params.id)
+//     if (student) {
+//       res.send(student)
+//     } else {
+//       res.status(404).send('Student not found')
+//     }
+//   } catch (error) {
+//     next(error)
+//   }
+// })
+router.get('/:id', async (req, res, next) => {
+    try{
+      let test = await Test.findByPk(req.params.id)
+      if(test){
+          res.send(test)
+        } else {
+          res.status(404).send('Student not found')
+        }
+      } catch(error){
+        next(error)
+    }
+})
 
 module.exports = router;
+
